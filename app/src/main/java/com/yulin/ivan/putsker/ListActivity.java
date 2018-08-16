c9bc8d8793a09e0e3e69a84634c5c49b858febcc
tyizchak <taliayit@gmail.com>
2018-08-16 20:08:00 +0300

    lala

--- app/src/main/java/com/yulin/ivan/putsker/ListActivity.java
+++ app/src/main/java/com/yulin/ivan/putsker/ListActivity.java
@@ -1,8 +1,11 @@
 package com.yulin.ivan.putsker;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -19,6 +22,7 @@ import java.util.Map;
 public class ListActivity extends AppCompatActivity {
 
     ListView listView;
+    //ListView coursesList;
     FirebaseDatabase db;
     DatabaseReference ref;
     ArrayList<String> list;
@@ -33,6 +37,7 @@ public class ListActivity extends AppCompatActivity {
 
         guide = new Guide();
         listView = (ListView) findViewById(R.id.listview);
+        //coursesList = (ListView) findViewById(R.id.coursesList);
         db = FirebaseDatabase.getInstance();
         ref = db.getReference().child("Guides");
         //todo set write permissions for Guides
@@ -48,6 +53,7 @@ public class ListActivity extends AppCompatActivity {
                         //Get map of users in datasnapshot
                         collectGuidesData((Map<String, Object>) dataSnapshot.getValue());
                         adapter.notifyDataSetChanged();
+                        Object o = dataSnapshot.getValue();
                     }
 
                     @Override
@@ -55,10 +61,19 @@ public class ListActivity extends AppCompatActivity {
                         //handle databaseError
                     }
                 });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(ListActivity.this, CoursesActivity.class);
+                intent.putExtra("index", position);
+                //need to send courses data
+            }
+        });
+
     }
 
     private void collectGuidesData(Map<String, Object> guides) {
-
 
         //iterate through each user, ignoring their UID
         for (Map.Entry<String, Object> entry : guides.entrySet()) {
@@ -68,4 +83,5 @@ public class ListActivity extends AppCompatActivity {
             list.add((String) singleUser.get("name"));
         }
     }
+
 }
